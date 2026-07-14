08511c26b498a84dd8dbb6d23b3f3b8d99f57dc8
Mariam Hany <mariamhanymakram@gmail.com>
2026-07-14 22:12:00 +0300

    Add password verification loop in Fixed Password.cpp

---
+++ Sheet #2 (Loops)/D. Fixed Password.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	while (true) {
+		int N;
+		cin >> N;
+		if (N == 1999) {
+			cout << "Correct" << endl;
+			return 0;
+		}
+		else {
+			cout << "Wrong" << endl;
+		}
+	}
+
+}
